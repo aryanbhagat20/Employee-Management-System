0674613f2f909c0011d1f91228ff6400b9296666
Aryan Bhagat <aryanbhagat20@gmail.com>
2025-03-31 01:31:10 +0530

    Employee entity was created

---
+++ src/main/java/com/aryan/employee_management_system/entity/Employee.java
@@ -0,0 +1,30 @@
+package com.aryan.employee_management_system.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "employee")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    private String phone;
+
+}
