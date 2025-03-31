e6efdbc902b6bb47a1c3a5b79a54959bdf56bf64
Aryan Bhagat <aryanbhagat20@gmail.com>
2025-03-31 15:20:32 +0530

    Employee Services

--- src/main/java/com/aryan/employee_management_system/service/EmployeeService.java
+++ src/main/java/com/aryan/employee_management_system/service/EmployeeService.java
@@ -3,41 +3,39 @@ package com.aryan.employee_management_system.service;
 
 import com.aryan.employee_management_system.entity.Employee;
 import com.aryan.employee_management_system.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
+@Service
 public class EmployeeService {
 
+    @Autowired
     private EmployeeRepository employeeRepository;
 
-    @PostMapping("/addEmployee")
     public Employee saveEmployee(Employee employee) {
         return employeeRepository.save(employee);
     }
 
-    @PostMapping("/addEmployees")
     public List<Employee> saveEmployees(List<Employee> employees) {
         return employeeRepository.saveAll(employees);
     }
 
-    @GetMapping("/getAllEmployees")
     public List<Employee> getEmployees() {
         return employeeRepository.findAll();
     }
 
-    @GetMapping("/getEmployeeById/{id}")
     public Employee getEmployeeById(Long id) {
         return employeeRepository.findById(id).orElse(null);
     }
 
-    @GetMapping("/getEmployeeByName/{name}")
-    public Employee getEmployeeByName(String name) {
-        return employeeRepository.findByName(name);
+    public Employee getEmployeeByName(String firstName) {
+        return employeeRepository.findByFirstName(firstName);
     }
 
-    @GetMapping("/deleteEmployee/{id}")
     public String deleteEmployee(Long id) {
         employeeRepository.deleteById(id);
         return "Employee removed !! " + id;
