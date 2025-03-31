303cf8b83a3f94411c07b43e0725485a24724626
Aryan Bhagat <aryanbhagat20@gmail.com>
2025-03-31 15:20:48 +0530

    Employee Repository

--- src/main/java/com/aryan/employee_management_system/repository/EmployeeRepository.java
+++ src/main/java/com/aryan/employee_management_system/repository/EmployeeRepository.java
@@ -4,5 +4,5 @@ import com.aryan.employee_management_system.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-    Employee findByName(String name);
+    Employee findByFirstName(String FirstName);
 }
